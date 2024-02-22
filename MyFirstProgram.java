cbbd48325d9922c0af163d558aad80bd46cd690b
Ihor Ivaniuk <123873884+ivaniukihornik@users.noreply.github.com>
2024-02-22 13:28:20 +0200

    Fixed MyFirstProgram.java

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -1,6 +1,6 @@
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("I'm a student of 12221 group #6");
-        System.out.println("My next class is in a few minutes");
+        System.out.println("I'm a student of 12221 group #7");
+        System.out.println("My next class is in 30 minutes");
     }
 }
